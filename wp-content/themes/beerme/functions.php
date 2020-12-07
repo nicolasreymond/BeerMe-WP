66159a5c1ba2bb53f9a1543369cc495a7ed01f8e
nicolasreymond <n.reymond.12@gmail.com>
2020-12-07 15:37:57 +0100

    [Style] Change logo from login page

--- wp-content/themes/beerme/functions.php
+++ wp-content/themes/beerme/functions.php
@@ -257,3 +257,28 @@ function innofit_register_required_plugins() {
 
 	tgmpa( $plugins, $config );
 }
+
+add_filter('wp_nav_menu_items', 'add_login_logout_link', 10, 2);
+function add_login_logout_link($items, $args)
+{
+    ob_start();
+    wp_loginout('index.php');
+    $loginoutlink = ob_get_contents();
+    ob_end_clean();
+    $items .= '<li>'. $loginoutlink .'</li>';
+    return $items;
+}
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/site-login-logo.png);
+			height:65px;
+			width:320px;
+			background-size: 320px 65px;
+			background-repeat: no-repeat;
+        	padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action('login_enqueue_scripts', 'my_login_logo');
